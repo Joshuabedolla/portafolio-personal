6b7a9a8491bad9765e42990eb37ef032ccc145bb
Joshuabedolla <moralesbedollajoshuazaquiel@gmail.com>
2025-06-28 12:43:20 -0600

    Correcion1

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
+// https://github.com/Joshuabedolla/portafolio-personal.git
 export default defineConfig({
   plugins: [react()],
 })
